2b75431b5cd8cd79a783e467b66ed5240cacffb5
Manoj Mathai <manomathai@gmail.com>
2020-06-11 14:57:44 +0530

    Jenkins change 4

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    project = "host-project-mm"
+#    project = "host-project-mm"
     bucket  = "mm-tf-statefiles"
     prefix  = "terraform/state"
     credentials = "./creds/serviceaccount.json"
